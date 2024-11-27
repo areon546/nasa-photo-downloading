ebd130049d60312477d98ec942c5eae203e5e5dd
areon546 <57325743+areon546@users.noreply.github.com>
2024-11-27 11:33:57 +0000

    Updated getJson to use classes more formally

--- README.md
+++ README.md
@@ -4,3 +4,12 @@
 - there are two programs to run:
 - [command->txtFile](getJsonIntoTXT.py) - constructs a text file with a lot of links that the user can later use to download photos from
 - [downloadFiles](downloadFiles.py) - downloads the contents of specified websites in the given text file
+
+
+
+
+links to use:
+- https://api.nasa.gov/
+- https://epic.gsfc.nasa.gov/about/api
+- https://github.com/corincerami/mars-photo-api/tree/master?tab=readme-ov-file
+
--- getJsonIntoTXT.py
+++ getJsonIntoTXT.py
@@ -1,6 +1,39 @@
 import requests
 from enum import Enum
 
+
+# {"curiosity" : 0,"opportunity" : 0, "spirit" : 0, "name" : ""}
+
+# Perseverance rover
+# Abbreviation 	Camera
+# EDL_RUCAM 	Rover Up-Look Camera
+# EDL_RDCAM 	Rover Down-Look Camera
+# EDL_DDCAM 	Descent Stage Down-Look Camera
+# EDL_PUCAM1 	Parachute Up-Look Camera A
+# EDL_PUCAM2 	Parachute Up-Look Camera B
+# NAVCAM_LEFT 	Navigation Camera - Left
+# NAVCAM_RIGHT 	Navigation Camera - Right
+# MCZ_RIGHT 	Mast Camera Zoom - Right
+# MCZ_LEFT 	Mast Camera Zoom - Left
+# FRONT_HAZCAM_LEFT_A 	Front Hazard Avoidance Camera - Left
+# FRONT_HAZCAM_RIGHT_A 	Front Hazard Avoidance Camera - Right
+# REAR_HAZCAM_LEFT 	Rear Hazard Avoidance Camera - Left
+# REAR_HAZCAM_RIGHT 	Rear Hazard Avoidance Camera - Right
+# SKYCAM 	MEDA Skycam
+# SHERLOC_WATSON 	SHERLOC WATSON Camera
+
+# Abbreviation 	    Camera 	                                            Curiosity 	Opportunity 	Spirit
+# FHAZ 	            Front Hazard Avoidance Camera 	                    ✔       	✔ 	            ✔
+# RHAZ 	            Rear Hazard Avoidance Camera 	                    ✔ 	        ✔           	✔
+# MAST 	            Mast Camera 	                                    ✔ 		
+# CHEMCAM           Chemistry and Camera Complex 	                    ✔ 		
+# MAHLI 	        Mars Hand Lens Imager 	                            ✔ 		
+# MARDI 	        Mars Descent Imager 	                            ✔ 		
+# NAVCAM 	        Navigation Camera 	                                ✔ 	        ✔ 	            ✔
+# PANCAM 	        Panoramic Camera 		                                        ✔   	        ✔
+# MINITES           Miniature Thermal Emission Spectrometer (Mini-TES) 		        ✔ 	            ✔
+
+
 class APIS(Enum):
     EPIC = "https://epic.gsfc.nasa.gov/api/"
     CURIOSITY_ROVER = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos"
@@ -44,59 +77,53 @@ class APIS(Enum):
             "SKYCAM" : "MEDA Skycam", 
             "SHERLOC_WATSON" : "SHERLOC WATSON Camera"}
 
-# {"curiosity" : 0,"opportunity" : 0, "spirit" : 0, "name" : ""}
-
-# Perseverance rover
-# Abbreviation 	Camera
-# EDL_RUCAM 	Rover Up-Look Camera
-# EDL_RDCAM 	Rover Down-Look Camera
-# EDL_DDCAM 	Descent Stage Down-Look Camera
-# EDL_PUCAM1 	Parachute Up-Look Camera A
-# EDL_PUCAM2 	Parachute Up-Look Camera B
-# NAVCAM_LEFT 	Navigation Camera - Left
-# NAVCAM_RIGHT 	Navigation Camera - Right
-# MCZ_RIGHT 	Mast Camera Zoom - Right
-# MCZ_LEFT 	Mast Camera Zoom - Left
-# FRONT_HAZCAM_LEFT_A 	Front Hazard Avoidance Camera - Left
-# FRONT_HAZCAM_RIGHT_A 	Front Hazard Avoidance Camera - Right
-# REAR_HAZCAM_LEFT 	Rear Hazard Avoidance Camera - Left
-# REAR_HAZCAM_RIGHT 	Rear Hazard Avoidance Camera - Right
-# SKYCAM 	MEDA Skycam
-# SHERLOC_WATSON 	SHERLOC WATSON Camera
-
-# Abbreviation 	    Camera 	                                            Curiosity 	Opportunity 	Spirit
-# FHAZ 	            Front Hazard Avoidance Camera 	                    ✔       	✔ 	            ✔
-# RHAZ 	            Rear Hazard Avoidance Camera 	                    ✔ 	        ✔           	✔
-# MAST 	            Mast Camera 	                                    ✔ 		
-# CHEMCAM           Chemistry and Camera Complex 	                    ✔ 		
-# MAHLI 	        Mars Hand Lens Imager 	                            ✔ 		
-# MARDI 	        Mars Descent Imager 	                            ✔ 		
-# NAVCAM 	        Navigation Camera 	                                ✔ 	        ✔ 	            ✔
-# PANCAM 	        Panoramic Camera 		                                        ✔   	        ✔
-# MINITES           Miniature Thermal Emission Spectrometer (Mini-TES) 		        ✔ 	            ✔
 
 fileWrittenTo = "links.txt"
-API_IN_USE = APIS.EPIC
+API_IN_USE = APIS.CURIOSITY_ROVER
 
 
+class APIQuery():
+    
+    def __init__(self, API, params) -> None:
+        self.api = API
+        self.params = self.initialParams(params)
+        pass
+    
+    def initialParams(self, paramInput):
+        paramInput["API_KEY"] = "DEMO_KEY"
+        
+        
+        # add params based on API
+        """
+        my thought process on how to do this is basically that Id need to check the manifest of the API for number of days, earth and otherwise, 
+        
+        """
+        
+        return
+    
+    def askCamera():
+        return
+    
+    def sendQuery(self):
+        return
+    
+    def writeToFile(htmlResponse):
 
-def writeToFile(htmlResponse):
+            try:
+                jsonRes = htmlResponse.json() # json output of text file
+            except requests.JSONDecodeError:
+                print("Invalid API keys entered")
+                return
+            else:
+                with open(fileWrittenTo, "w") as file:
+                    # print(htmlResponse.content)
 
-        try:
-            jsonRes = htmlResponse.json() # json output of text file
-        except requests.JSONDecodeError:
-            print("Invalid API keys entered")
-            return
-        else:
-            with open(fileWrittenTo, "w") as file:
-                print(htmlResponse.content)
+                    # print(jsonRes["photos"][0]["img_src"])
+                    # print(len(jsonRes["photos"]))
 
-                print(jsonRes["photos"][0]["img_src"])
-                print(len(jsonRes["photos"]))
-
-                for i in range (len(jsonRes["photos"])):
-                    file.write(f"{jsonRes["photos"][i]["img_src"]}\n")
-                    # print(i)
+                    for i in range (len(jsonRes["photos"])):
+                        file.write(f"{jsonRes["photos"][i]["img_src"]}\n")
+                        # print(i)
 
 def main(params):
     # so i wanna do a get request of this github link
